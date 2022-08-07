8942764ae18fb755277ea6d8a446ecade63b50e6
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-07 22:18:30 +0900

    music 追加

---
+++ MyMusic/Shared/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  Shared
+//
+//  Created by 河内　光太 on 2022/08/07.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
