555eddf6aedfe06cb3961a27c67bdd9fe9002b5a
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-07 10:03:01 +0900

    2022/08/07　janeken first

---
+++ Myjanken2/Shared/Myjanken2App.swift
@@ -0,0 +1,17 @@
+//
+//  Myjanken2App.swift
+//  Shared
+//
+//  Created by 河内　光太 on 2022/08/07.
+//
+
+import SwiftUI
+
+@main
+struct Myjanken2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
